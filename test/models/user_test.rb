a2529f0539aceb5517993ed441dc7a14b38f3f1c
Paulo Arthur Venturi <paulo.venturi@gmail.com>
2013-07-12 16:33:36 -0300

    working on tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "an user should enter a first name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:first_name].empty?
+  end
+
+  test "an user should enter a last name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:last_name].empty?
+  end
+
+  test "an user should enter a profile name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
+
+  test "an user should have an unique profile name" do
+  	user = User.new
+  	user.profile_name = users(:paulo).profile_name
+
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+  test "an user should have a profile name without spaces" do
+    user = User.new
+    user.profile_name = "paulo venturi"
+
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+    assert user.errors[:profile_name].include?("Must be formatted correctly.")
+  end
 end
