285dc508b8a28d53ac5e3fb294d88e5d9dc52000
Paulo Arthur Venturi <paulo.venturi@gmail.com>
2013-07-27 00:16:35 -0300

    fixed user unit test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -35,4 +35,16 @@ class UserTest < ActiveSupport::TestCase
     assert !user.errors[:profile_name].empty?
     assert user.errors[:profile_name].include?("Must be formatted correctly.")
   end
+
+  test "user with a correct profile name" do
+    user = User.new(first_name: "Paulo", last_name: "Venturi", email: "haha@gmail.com")
+    user.password = "abc12345678"
+    user.password_confirmation = "abc12345678"
+
+    user.profile_name = "kruds"
+
+    assert user.valid?
+
+  end
+
 end
