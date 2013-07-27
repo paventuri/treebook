8ece50dc6d078a5f0d9a047e1162d303eff5a64d
Paulo Arthur Venturi <paulo.venturi@gmail.com>
2013-07-26 23:50:46 -0300

    working on unit tests for status

--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -23,7 +23,15 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should be logged in to post a status" do
+    post :create, status: {content: "hello"}
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
   test "should create status" do
+    sign_in users(:paulo)
+
     assert_difference('Status.count') do
       post :create, status: { content: @status.content}
     end
@@ -36,12 +44,26 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should be logged in to edit a status" do
+    get :edit, id: @status
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
   test "should get edit" do
+    sign_in users(:paulo)
     get :edit, id: @status
     assert_response :success
   end
 
+  test "should be logged in to update a status" do
+    get :update, id: @status, status: {content: "hello update"}
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
   test "should update status" do
+    sign_in users(:paulo)
     patch :update, id: @status, status: { content: @status.content}
     assert_redirected_to status_path(assigns(:status))
   end
